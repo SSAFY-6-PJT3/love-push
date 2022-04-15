71b6ff3025e807c726a5924f0aa576e00ea7d7b1
eora21 <eora21@naver.com>
2022-04-15 13:38:06 +0900

    feat: upload backend

---
+++ backend/joalarm/src/test/java/com/cupid/joalarm/JoalarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupid.joalarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoalarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
