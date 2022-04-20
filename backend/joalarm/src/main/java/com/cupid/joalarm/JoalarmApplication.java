cb0450cafb5c307a968d3a566ab37e6583e87cf2
eora21 <eora21@naver.com>
2022-04-20 10:47:56 +0900

    feat: 🎸 chat system
    채팅 시스템을 구현하였습니다. 메세지 및 채팅방의 기본 규칙을 잡아놨으며, 추후 변동될 수 있습니다.

--- backend/joalarm/src/main/java/com/cupid/joalarm/JoalarmApplication.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/JoalarmApplication.java
@@ -2,8 +2,9 @@ package com.cupid.joalarm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class JoalarmApplication {
 
 	public static void main(String[] args) {
