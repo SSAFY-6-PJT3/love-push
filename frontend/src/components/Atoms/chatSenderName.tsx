85c3cedac25d5d1d5e61db6babf5fbebf924647e
constantine <glowforever42@gmail.com>
2022-04-21 15:10:23 +0900

    feat: 🎸 chat system Lobby init

---
+++ frontend/src/components/Atoms/chatSenderName.tsx
@@ -0,0 +1,9 @@
+const ChatSenderName = () => {
+  return (
+    <div>
+      <h1>익명의 시라소니</h1>
+    </div>
+  )
+}
+
+export default ChatSenderName;
---
+++ frontend/src/components/Molecules/chatBox.tsx
@@ -0,0 +1,23 @@
+import ChatProfileEmoji from "../Atoms/chatProfileEmoji"
+import ChatButton from "../Atoms/chatButton"
+import ChatRecentMessage from "../Atoms/chatRecentMessage"
+import ChatLeftMessege from "../Atoms/chatLeftMessage"
+import ChatReport from "../Atoms/chatReport"
+import ChatSenderName from "../Atoms/chatSenderName"
+import ChatTime from "../Atoms/chatTime"
+
+const ChatBox = () => {
+  return (
+    <div>
+      <ChatProfileEmoji />
+      <ChatSenderName />
+      <ChatTime />
+      <ChatRecentMessage />
+      <ChatLeftMessege />
+      <ChatButton />
+      <ChatReport />
+    </div>
+  )
+}
+
+export default ChatBox
