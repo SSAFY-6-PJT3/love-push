f91e4e1c49dc3c21e7e64afd6bd2903b0fee7041
82surf <82surf@gmail.com>
2022-04-26 23:06:27 +0900

    feat: 🎸 [Frontend] 회원가입 유효성 검사
    ✅ Closes: #S06P31B206-353

--- frontend/src/components/Templetes/SignupForm.tsx
+++ frontend/src/components/Templetes/SignupForm.tsx
@@ -5,26 +5,34 @@ import { SignupInput } from '../Atoms/Inputs';
 
 interface IProps_SignupForm {
   label: string;
+  value: string;
+  type: string;
+  onInputChange: (val: string) => void;
   onFormSubmit: (val: string) => void;
 }
 
-const SignupForm = ({ label, onFormSubmit }: IProps_SignupForm) => {
-  let inputVal = '';
-
+const SignupForm = ({
+  label,
+  value,
+  type,
+  onInputChange,
+  onFormSubmit,
+}: IProps_SignupForm) => {
   const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    inputVal = e.target.value;
+    onInputChange(e.target.value);
   };
 
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onFormSubmit(inputVal);
+    onFormSubmit(value);
   };
 
   return (
     <StyledForm onSubmit={submitHandler}>
       <StyledLabel>{label}</StyledLabel>
       <SignupInput
-        type="text"
+        type={type}
+        value={value}
         fontSize="20px"
         fontWeight="700"
         onChange={inputChangeHandler}
@@ -38,7 +46,7 @@ const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin: 0 2rem 5rem;
+  margin: 0 2rem 2rem;
 `;
 
 const StyledLabel = styled.label`
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -16,7 +16,13 @@ const Signup = () => {
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [errMsg, setErrMsg] = useState('');
 
+  // 아이디, 비밀번호 정규식
+  const userIdRegExp = /^[a-z0-9]{6,16}$/;
+  const passwordRegExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,16}$/;
+
+  // 회원가입 API 호출
   const callSignUpAPI = () => {
     const signupInfo = {
       emoji: 'string',
@@ -33,40 +39,84 @@ const Signup = () => {
       });
   };
 
-  const formSubmitHandler = (v: string) => {
+  const inputChangeHandler = (v: string) => {
     switch (pageId) {
       case '1':
         setUserId(v);
-        navigate('/signup/2');
         break;
       case '2':
         setPassword(v);
-        navigate('/signup/3');
         break;
       case '3':
         setPasswordConfirm(v);
-        callSignUpAPI();
         break;
     }
   };
+
+  const formSubmitHandler = (v: string) => {
+    switch (pageId) {
+      // 아이디 입력 페이지
+      case '1':
+        setUserId(v);
+        if (userIdRegExp.test(v)) {
+          navigate('/signup/2');
+          setErrMsg('');
+        } else {
+          setErrMsg('6자 이상, 16자 이하의 영문 혹은 숫자로 입력해주세요.');
+        }
+        break;
+      // 비밀번호 입력 페이지
+      case '2':
+        setPassword(v);
+        if (passwordRegExp.test(v)) {
+          navigate('/signup/3');
+          setErrMsg('');
+        } else {
+          setErrMsg('6자 이상, 16자 이하의 영문, 숫자 조합으로 입력해주세요.');
+        }
+        break;
+      // 비밀번호 확인 페이지
+      case '3':
+        setPasswordConfirm(v);
+        if (password === passwordConfirm) {
+          callSignUpAPI();
+        } else {
+          setErrMsg('비밀번호가 일치하지 않습니다.');
+        }
+        break;
+    }
+  };
+
+  // const goBack = () => {
+  //   navigate(-1);
+  // };
 
   return (
     <Wrapper>
       {pageId === '1' && (
         <SignupForm
           label="아이디를 입력해주세요."
+          type="text"
+          value={userId}
+          onInputChange={inputChangeHandler}
           onFormSubmit={formSubmitHandler}
         />
       )}
       {pageId === '2' && (
         <SignupForm
           label="비밀번호를 입력해주세요."
+          type="password"
+          value={password}
+          onInputChange={inputChangeHandler}
           onFormSubmit={formSubmitHandler}
         />
       )}
       {pageId === '3' && (
         <SignupForm
           label="비밀번호를 한 번 더 입력해주세요."
+          type="password"
+          value={passwordConfirm}
+          onInputChange={inputChangeHandler}
           onFormSubmit={formSubmitHandler}
         />
       )}
@@ -74,6 +124,7 @@ const Signup = () => {
         <SignupResult>회원가입이 완료되었습니다.</SignupResult>
       )}
       {pageId === '9' && <SignupResult>회원가입에 실패했습니다.</SignupResult>}
+      {errMsg && <ErrMsg>{errMsg}</ErrMsg>}
     </Wrapper>
   );
 };
@@ -91,6 +142,13 @@ const SignupResult = styled.h1`
   color: white;
   font-weight: 700;
   margin-bottom: 3rem;
+`;
+
+const ErrMsg = styled.p`
+  font-size: 14px;
+  text-align: center;
+  font-weight: 300;
+  color: red;
 `;
 
 export default Signup;
