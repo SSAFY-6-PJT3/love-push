3131075d5129f930d06be040a096aed5efbd12b8
constantine <glowforever42@gmail.com>
2022-04-27 15:18:26 +0900

    feat: 🎸 Text 컴포넌트 생성

---
+++ frontend/src/components/Atoms/Text.tsx
@@ -0,0 +1,98 @@
+import styled from 'styled-components';
+
+interface TextStyleProps {
+  margin?: string;
+  width?: string;
+}
+
+const ChatTitle = styled.text<TextStyleProps>`
+margin: ${(props) => props.margin};
+width: ${(props) => props.width || '100%'};
+font-size: 2rem;
+font-weight: bold;
+display: flex;
+margin: 1rem 0;
+justify-content: center;
+background-color: #EEF8FF;
+`;
+
+const LoginTitle = styled.text<TextStyleProps>`
+margin: ${(props) => props.margin};
+width: ${(props) => props.width || '100%'};
+font-size: 2rem;
+font-weight: bold;
+display: flex;
+margin: 1rem 0;
+justify-content: center;
+background-color: #EEF8FF;
+`;
+
+const LeftMessageCount = styled.div<TextStyleProps>`
+  margin: ${(props) => props.margin};
+  width: ${(props) => props.width || '100%'};
+  display: flex;
+  height: 2rem;
+  width: 2rem;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: #71E155;
+  color: white;
+  margin: 1rem;
+  border: none;
+`;
+
+const RecentMessage = styled.text<TextStyleProps>`
+  margin: ${(props) => props.margin};
+  width: ${(props) => props.width || '100%'};
+  font-size: 1rem;
+  display: flex;
+  margin: 1rem 0;
+  color: #8E8E8E;
+`;
+
+const RandomNickname = styled.text<TextStyleProps>`
+  margin: ${(props) => props.margin};
+  width: ${(props) => props.width || '100%'};
+  font-size: 1.2rem;
+  font-weight: bold;
+  display: flex;
+  margin: 1rem 0;
+`;
+
+const Timeline = styled.text<TextStyleProps>`
+margin: ${(props) => props.margin};
+width: ${(props) => props.width };
+font-size: 0.5rem;
+display: flex;
+margin: 1rem 0;
+color: #8E8E8E;
+justify-content: center;
+`;
+
+const OtherUserChatText = styled.text<TextStyleProps>`
+margin: ${(props) => props.margin};
+width: ${(props) => props.width};
+padding: 8px 16px;
+border-radius: 10px;
+border: none;
+font-weight: 300;
+background-color: #FFFFFF;
+box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);  
+`;
+
+const MyChatText = styled.text<TextStyleProps>`
+margin: ${(props) => props.margin};
+width: ${(props) => props.width};
+padding: 8px 16px;
+border-radius: 10px;
+border: none;
+font-weight: 300;
+background-color: #FFFFFF;
+box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);  
+margin-right : 1rem;
+`;
+
+
+export { ChatTitle, LeftMessageCount, RecentMessage, RandomNickname, Timeline, LoginTitle, OtherUserChatText, MyChatText };
