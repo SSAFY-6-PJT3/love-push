1db06ea98f3a9a8d045cf938fbabd70fba2b1292
EuneeChung <chungee375@gmail.com>
2022-05-06 18:52:59 +0900

    feat: 🎸 [AOS] Add check permissions

--- android/joalarm/app/src/main/java/com/cupid/joalarm/MainActivity.kt
+++ android/joalarm/app/src/main/java/com/cupid/joalarm/MainActivity.kt
@@ -1,20 +1,69 @@
 package com.cupid.joalarm
 
-import android.content.Intent
-import android.net.Uri
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSION_ACCESS_ALL = 100
+
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val webView : WebView = findViewById(R.id.web_view)
+        checkPermission()
+
+    }
+
+
+    private fun checkPermission() {
+        for (permission in permissions) {
+            // 권한 여부 체크
+            val check = checkCallingOrSelfPermission(permission)
+            if (check == PackageManager.PERMISSION_DENIED) {
+                // 권한 거부 상태라면, 사용자에게 권한 허용여부를 확인하는 창을 띄움
+                // 허가가 안되어 있는 권한을 요청한다.
+                ActivityCompat.requestPermissions(this, permissions, PERMISSION_ACCESS_ALL)
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+        if (requestCode === PERMISSION_ACCESS_ALL) {
+            if (grantResults.size > 0) {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) System.exit(0)
+                }
+                openWebPage("https://www.someone-might-like-you.com/mainPage")
+            }
+        }
+    }
+
+
+    fun openWebPage(url: String) {
+        val webView: WebView = findViewById(R.id.web_view)
 
         webView.webViewClient = WebViewClient() // 클릭시 새창 안뜨게
 //        webView.webChromeClient= WebChromeClient()
@@ -32,19 +81,6 @@ class MainActivity : AppCompatActivity() {
         mWebSettings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN // 컨텐츠 사이즈 맞추기
         mWebSettings.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐시 허용 여부
         mWebSettings.domStorageEnabled = true // 로컬저장소 허용 여부
-
-        webView.loadUrl("https://www.someone-might-like-you.com/")
-//        webView.loadUrl("http://3.35.170.113:31000/mainpage")
-//        webView.loadUrl("https://www.naver.com/")
-//        webView.loadUrl("https://www.google.com/")
-
-//        openWebPage("https://www.someone-might-like-you.com/mainPage")
-    }
-    fun openWebPage(url: String) {
-        val webpage: Uri = Uri.parse(url)
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
+        webView.loadUrl(url)
     }
 }
