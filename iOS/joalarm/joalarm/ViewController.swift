07d85d893b29c5de32e2443b936be5b06e0656a1
whee <glowforever42@gmail.com>
2022-05-11 11:36:56 +0900

    feat: 🎸 [iOS] 앱이 백그라운드로 가도 위치 정보 제공 기능 추가

--- iOS/joalarm/joalarm/ViewController.swift
+++ iOS/joalarm/joalarm/ViewController.swift
@@ -21,15 +21,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
 
         // 아래 함수 요청시 위치권한 팝업 출력
         self.locationManager.requestWhenInUseAuthorization()
-        
-//        getLocationUsagePermission()
-
-        
+    
         // 스와이프를 통해서 뒤로가기 앞으로가기를 할수 있게 해주는 설정값
         self.webKitView?.allowsBackForwardNavigationGestures = true
+        
+        // 정확한 위치 요청
         locationManager.desiredAccuracy =
 kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        
+        // 앱이 백그라운드 상태에서 위치가 변경되어도 추적
+        // 배터리 이슈가 존재 할 수도
+        locationManager.allowsBackgroundLocationUpdates = true
+        // 앱을 종료하면 백그라운드에서 더 이상 위치 안씀 -> 설명이 따로 필요한지?
         
     let space = locationManager.location?.coordinate
     lat = space?.latitude
@@ -75,21 +79,19 @@ kCLLocationAccuracyBest
     }
 }
 
-
-
-
 extension ViewController {
     func getLocationUsagePermission() {
         self.locationManager.requestWhenInUseAuthorization()
     }
     
-    // 위치 정보 권한 없을 시, 설정 창으로 보내주는 코드
+    // 위치 정보 권한 없을 시, 앱 설정 창으로 보내주는 코드
     func setLocationAuth() {
         let authAlertController: UIAlertController
         authAlertController = UIAlertController(title: "위치 정보 권한 요청", message: "저희 서비스를 이용하시기 위해서는 위치 정보가 필요합니다. 위치 접근 허용을 해주세요.", preferredStyle: .alert)
         // 앱 설명에 필수권한으로 적어두기.
         let getAuthAction: UIAlertAction
         getAuthAction = UIAlertAction(title: "네 알겠습니다.", style: UIAlertAction.Style.default, handler: { _ in
+            // 보내주면 해당 앱의 설정 창으로 가게 된다. 가능하다면 설명을 자세히 적어 주기.
             if let appSettings = URL(string: UIApplication.openSettingsURLString){
                 UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
             }
@@ -100,23 +102,17 @@ extension ViewController {
 
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
             switch status {
-            case.authorizedAlways, .authorizedWhenInUse:
-                print("GPS 권한 설정 완료")
-            case.restricted, .notDetermined:
-//                self.locationManager.requestWhenInUseAuthorization()
-                print("GPS 권한 설정 되지 않음")
+//            case.authorizedAlways, .authorizedWhenInUse:
+//                print("GPS 권한 설정 완료")
+//            case.restricted, .notDetermined:
+//                print("GPS 권한 설정 되지 않음")
             case.denied:
-                self.locationManager.requestWhenInUseAuthorization()
-                print("GPS 권한 거부 됨")
+//                print("GPS 권한 거부 됨") 굳이 케이스 다 필요없이 디나이랑 디폴트만 있으면 될듯. 스위치는 해야하니까.
                 setLocationAuth()
             default:
                 print("GPS Default")
             }
         }
-    
-// 앱이 백그라운드 상태에서 위치가 변경되어도 추적
-//    let locationBackground = CLLocationManager()
-//    locationBackground.allowsBackgroundLocationUpdates = true
 }
 
 
