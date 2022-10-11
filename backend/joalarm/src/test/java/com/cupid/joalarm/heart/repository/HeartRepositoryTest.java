4e1e975654ece72559a2056686637fb59b23ebbb
eora21 <eora21@naver.com>
2022-10-11 15:27:48 +0900

    test: 💍 Heart 리포지토리 테스트코드
    세 유저와 세 하트 교환 후 한 유저를 대상으로 하트를 교환한 타 유저들의 seq가 제대로 잡히는지 확인

---
+++ backend/joalarm/src/test/java/com/cupid/joalarm/heart/repository/HeartRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.cupid.joalarm.heart.repository;
+
+import com.cupid.joalarm.account.entity.Account;
+import com.cupid.joalarm.account.repository.AccountRepository;
+import com.cupid.joalarm.heart.entity.AccountsEmbedded;
+import com.cupid.joalarm.heart.entity.Heart;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.StringJoiner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Commit
+class HeartRepositoryTest {
+    @Autowired AccountRepository accountRepository;
+    @Autowired HeartRepository heartRepository;
+    @Autowired EntityManager em;
+
+    @Test
+    public void findReceiveMyHeartAccountsTest() throws Exception {
+
+//        given
+        Account account1 = new Account();
+        Account account2 = new Account();
+        Account account3 = new Account();
+        accountRepository.save(account1);
+        accountRepository.save(account2);
+        accountRepository.save(account3);
+
+        Heart heart1 = Heart.builder()
+                .AccountsWhoExchangedHearts(
+                        new AccountsEmbedded(
+                                account1, account2
+                        )
+                ).build();
+
+        Heart heart2 = Heart.builder()
+                .AccountsWhoExchangedHearts(
+                        new AccountsEmbedded(
+                                account1, account3
+                        )
+                ).build();
+
+        Heart heart3 = Heart.builder()
+                .AccountsWhoExchangedHearts(
+                        new AccountsEmbedded(
+                                account2, account3
+                        )
+                ).build();
+
+        heartRepository.save(heart1);
+        heartRepository.save(heart2);
+        heartRepository.save(heart3);
+
+//        when
+        em.flush();
+        em.clear();
+
+        System.out.println("=============================================================");
+        List<Long> receiveMyHeartAccountsSeq = heartRepository.findReceiveMyHeartAccountsSeq(account1.getAccountSeq());
+
+        //then
+        StringJoiner sb = new StringJoiner(", ");
+
+        for (Long receiveMyHeartAccountSeq : receiveMyHeartAccountsSeq) {
+            sb.add(Long.toString(receiveMyHeartAccountSeq));
+        }
+        System.out.println("receiveMyHeartAccountsSeq = " + sb);
+
+        assertThat(receiveMyHeartAccountsSeq).containsExactly(2L, 3L);
+    }
+}
