94fb08816c73a44176b2d8505c43077e89c8350d
Gkuer <gkuer95@gmail.com>
2022-11-05 23:53:45 +0900

    Update StompConfig.java

--- backend/joalarm/src/main/java/com/cupid/joalarm/config/StompConfig.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/config/StompConfig.java
@@ -28,7 +28,7 @@ public class StompConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-        .setAllowedOrigins("*")
+        .setAllowedOrigins("http://localhost:81")
         .withSockJS();
     }
 
