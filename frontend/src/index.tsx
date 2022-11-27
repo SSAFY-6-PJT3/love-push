539cd4ff433476bc316a9933f62388f9ba8e6a71
한승훈 <gkstmdgns422@naver.com>
2022-11-27 23:45:40 +0900

    fix: 🐛 콘텍스트 api 초기화

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,17 +6,15 @@ import { AuthContextProvider } from './store/authContext';
 import { AlertContextProvider } from './store/alertContext';
 import { ClientContextProvider } from './store/clientContext';
 
-
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore} from 'redux'
-import store from './redux/createReduxStore'
-
-
 ReactDOM.render(
   // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AlertContextProvider>
+    <AuthContextProvider>
+      <ClientContextProvider>
+        <App />
+      </ClientContextProvider>
+    </AuthContextProvider>
+  </AlertContextProvider>,
   // </React.StrictMode>,
   document.getElementById('root') as HTMLElement,
 );
