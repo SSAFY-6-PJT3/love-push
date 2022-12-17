1c6f01cc35dfabe2ecfa25024cd6bd585b636d8c
eora21 <eora21@naver.com>
2022-12-17 20:22:43 +0900

    feat: 🎸 하트 API 설명 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/heart/controller/HeartController.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/heart/controller/HeartController.java
@@ -3,6 +3,8 @@ package com.cupid.joalarm.heart.controller;
 import com.cupid.joalarm.heart.dto.SendHeartDTO;
 import com.cupid.joalarm.heart.entity.Heart;
 import com.cupid.joalarm.heart.service.HeartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,19 +13,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Api("heart 관련 기능")
 @RequiredArgsConstructor
 @Controller
 public class HeartController {
     private final HeartService heartService;
 
+    @ApiOperation(value = "하트 송신", notes = "하트를 보냅니다.")
     @MessageMapping("/heart")
     public void sendHeart(SendHeartDTO DTO) {
         heartService.sendHeart(DTO.getSendUser(), DTO.getSessionAccountHashMap());
     }
 
-    @GetMapping("/heart/sendheartlist")
-    public ResponseEntity<List<Long>> sendHeartList(long seq) {
+    @ApiOperation(value = "하트 보낸 리스트 조회", notes = "하트를 보낸 유저들을 조회합니다.")
+    @GetMapping("/heart/send_list")
+    public ResponseEntity<List<Long>> sendHeartList(@RequestParam("user") long seq) {
         return new ResponseEntity<>(heartService.SendHeartList(seq), HttpStatus.OK);
     }
 
