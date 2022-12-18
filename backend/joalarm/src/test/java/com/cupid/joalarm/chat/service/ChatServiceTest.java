0e5c8edbe62b802ca1ae4c656c190f64643134ad
eora21 <eora21@naver.com>
2022-12-18 16:06:38 +0900

    Revert "test: 💍 특정 채팅 이전의 채팅목록 받아오는 테스트 추가"
    This reverts commit 3cf29025b080febce35b027df99473aa0c84e605.

--- backend/joalarm/src/test/java/com/cupid/joalarm/chat/service/ChatServiceTest.java
+++ backend/joalarm/src/test/java/com/cupid/joalarm/chat/service/ChatServiceTest.java
@@ -6,10 +6,8 @@ import com.cupid.joalarm.account.entity.Account;
 import com.cupid.joalarm.account.repository.AccountRepository;
 import com.cupid.joalarm.chat.dto.ChatDTO;
 import com.cupid.joalarm.chat.entity.Chat;
-import com.cupid.joalarm.chat.repository.ChatRepository;
 import com.cupid.joalarm.chatroom.entity.Chatroom;
 import com.cupid.joalarm.chatroom.repository.ChatRoomRepository;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +20,6 @@ class ChatServiceTest {
     @Autowired ChatService chatService;
     @Autowired AccountRepository accountRepository;
     @Autowired ChatRoomRepository chatRoomRepository;
-    @Autowired ChatRepository chatRepository;
 
     @Test
     public void createChatTest() throws Exception {
@@ -50,33 +47,5 @@ class ChatServiceTest {
         assertThat(chatDTO.getMessage()).isEqualTo(chat.getMessage());
         assertThat(chatDTO.getSender()).isEqualTo(chat.getAccount().getAccountSeq());
         assertThat(chatDTO.getRoomId()).isEqualTo(chat.getChatroom().getSeq());
-    }
-
-    @Test
-    public void getChatTest() throws Exception {
-        //given
-        Chatroom chatroom = Chatroom.builder().build();
-        chatRoomRepository.save(chatroom);
-
-        long chatSeq = -1;
-
-        for (int i = 0; i < 30; i++) {
-            Chat chat = Chat.builder()
-                    .message("chat" + i)
-                    .chatroom(chatroom)
-                    .build();
-
-            chatRepository.save(chat);
-
-            chatSeq = chat.getSeq();
-        }
-
-        //when
-        List<ChatDTO> chatList = chatService.getChatList(chatroom.getSeq(), chatSeq);
-
-        //then
-        for (ChatDTO chatDTO : chatList) {
-            System.out.println("chatDTO.getMessage() = " + chatDTO.getMessage());
-        }
     }
 }
