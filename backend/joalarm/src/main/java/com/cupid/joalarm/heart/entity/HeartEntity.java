c190c89cf2c9e0513aaab281a1584a4377762c55
eora21 <eora21@naver.com>
2022-12-19 21:04:56 +0900

    feat: 🎸 HeartEntity를 RDB로 변경, 상대방의 성, 이름, 학교 사용

--- backend/joalarm/src/main/java/com/cupid/joalarm/heart/entity/HeartEntity.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/heart/entity/HeartEntity.java
@@ -1,19 +1,52 @@
 package com.cupid.joalarm.heart.entity;
 
+import com.cupid.joalarm.account.entity.Account;
+import com.cupid.joalarm.heart.dto.HeartDto;
+import com.cupid.joalarm.school.School;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
-@Document(collection = "heart")
-
 public class HeartEntity {
 
-    private long sendUser;
-    private long receiveUser;
+    @Id
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_seq")
+    private Account account;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "school_seq")
+    private School school;
+
+    public static HeartEntity convert(Account account, HeartDto heartDto, School school) {
+        return HeartEntity.builder()
+                .account(account)
+                .lastName(heartDto.getLoverLastName())
+                .firstName(heartDto.getLoverFirstName())
+                .school(school)
+                .build();
+    }
 }
