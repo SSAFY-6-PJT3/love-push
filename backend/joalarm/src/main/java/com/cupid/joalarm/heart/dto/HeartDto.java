f68d7c113210621e49846fcb563b8346b3709f67
eora21 <eora21@naver.com>
2022-12-19 21:08:25 +0900

    feat: 🎸 Dto 추가

---
+++ backend/joalarm/src/main/java/com/cupid/joalarm/heart/dto/HeartDto.java
@@ -0,0 +1,10 @@
+package com.cupid.joalarm.heart.dto;
+import lombok.Data;
+
+@Data
+public class HeartDto {
+    private Long accountSeq;
+    private String loverLastName;
+    private String loverFirstName;
+    private Long loverSchoolSeq;
+}
