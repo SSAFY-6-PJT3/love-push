00be62cbba5a9f68ef0b485c1556871e2454c013
eora21 <eora21@naver.com>
2022-12-20 23:05:03 +0900

    feat: 🎸 Swagger 내용 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/heart/controller/HeartController.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/heart/controller/HeartController.java
@@ -2,6 +2,8 @@ package com.cupid.joalarm.heart.controller;
 
 import com.cupid.joalarm.heart.dto.HeartDto;
 import com.cupid.joalarm.heart.service.HeartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,12 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("heart 관련 기능")
 @RequiredArgsConstructor
 @RestController
 public class HeartController {
 
     private final HeartService heartService;
 
+    @ApiOperation(value = "좋아하는 사람 지정", notes = "좋아하는 사람의 성, 이름, 학교를 등록 및 메시징을 날립니다.")
     @PostMapping("/heart")
     public ResponseEntity<HeartDto> sendHeart(@RequestBody HeartDto heartDto) {
         Optional<HeartDto> heartDtoOptional = heartService.setHeart(heartDto);
