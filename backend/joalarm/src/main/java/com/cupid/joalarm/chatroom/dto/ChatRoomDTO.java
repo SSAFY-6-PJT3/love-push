bc47b91558696a9b7330ef2c24f09b87b6b5159f
eora21 <eora21@naver.com>
2022-12-24 14:58:57 +0900

    refactor: 💡 Data 어노테이션 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/dto/ChatRoomDTO.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/dto/ChatRoomDTO.java
@@ -1,27 +1,14 @@
 package com.cupid.joalarm.chatroom.dto;
 
+import lombok.Data;
+
+@Data
 public class ChatRoomDTO {
     private Long pk;
     private Long[] users;
 
     public ChatRoomDTO(Long pk, Long[] users) {
         this.pk = pk;
-        this.users = users;
-    }
-
-    public Long getPk() {
-        return pk;
-    }
-
-    public void setPk(Long pk) {
-        this.pk = pk;
-    }
-
-    public Long[] getUsers() {
-        return users;
-    }
-
-    public void setUsers(Long[] users) {
         this.users = users;
     }
 
--- backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/dto/SubscribeChatRoomDTO.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/dto/SubscribeChatRoomDTO.java
@@ -1,5 +1,8 @@
 package com.cupid.joalarm.chatroom.dto;
 
+import lombok.Data;
+
+@Data
 public class SubscribeChatRoomDTO {
     private String type;
     private long person;
@@ -8,30 +11,6 @@ public class SubscribeChatRoomDTO {
     public SubscribeChatRoomDTO(String type, long person, long chatRoom) {
         this.type = type;
         this.person = person;
-        this.chatRoom = chatRoom;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public long getPerson() {
-        return person;
-    }
-
-    public void setPerson(long person) {
-        this.person = person;
-    }
-
-    public long getChatRoom() {
-        return chatRoom;
-    }
-
-    public void setChatRoom(long chatRoom) {
         this.chatRoom = chatRoom;
     }
 }
