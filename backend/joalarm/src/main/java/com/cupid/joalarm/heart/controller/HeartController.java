f730007dd6d393e780a7318f40558864c29b91a9
eora21 <eora21@naver.com>
2022-12-24 10:40:57 +0900

    feat: 🎸 하트 컨트롤러 생성

---
+++ backend/joalarm/src/main/java/com/cupid/joalarm/heart/controller/HeartController.java
@@ -0,0 +1,33 @@
+package com.cupid.joalarm.heart.controller;
+
+import com.cupid.joalarm.chatroom.dto.CreateChatRoomDTO;
+import com.cupid.joalarm.chatroom.service.ChatRoomService;
+import com.cupid.joalarm.heart.dto.HeartDto;
+import com.cupid.joalarm.heart.service.HeartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("heart")
+@Api(value = "heart 관련 기능")
+public class HeartController {
+
+    private final HeartService heartService;
+    private final ChatRoomService chatRoomService;
+
+    @ApiOperation(value = "하트를 받았을 때 호출합니다.", notes = "하트를 받으면 기록하고, 하트가 교환되었다면 채팅방을 생성합니다.")
+    @PostMapping
+    public void receiveHeart(HeartDto heartDto) {
+        if (heartService.receiveHeart(heartDto)) {
+            chatRoomService.CreateChatRoom(
+                    new CreateChatRoomDTO(heartDto.getSendAccountSeq(), heartDto.getReceiveAccountSeq()));
+        }
+
+    }
+
+}
