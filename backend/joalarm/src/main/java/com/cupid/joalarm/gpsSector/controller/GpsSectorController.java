046434fa7fbbbebe547f32aedac1ccab1505fab3
eora21 <eora21@naver.com>
2022-12-25 14:30:02 +0900

    feat: 🎸 세션을 통한 데이터 변경을 작성

--- backend/joalarm/src/main/java/com/cupid/joalarm/gpsSector/controller/GpsSectorController.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/gpsSector/controller/GpsSectorController.java
@@ -1,7 +1,6 @@
 package com.cupid.joalarm.gpsSector.controller;
 
 import com.cupid.joalarm.gpsSector.dto.EmojiDTO;
-import com.cupid.joalarm.gpsSector.dto.JoalarmDTO;
 import com.cupid.joalarm.gpsSector.dto.SectorDTO;
 import com.cupid.joalarm.gpsSector.service.GpsService;
 import lombok.RequiredArgsConstructor;
@@ -9,12 +8,11 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpAttributesContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @RequiredArgsConstructor
 @Controller
 public class GpsSectorController {
+
     private final GpsService gpsService;
 
     @MessageMapping("/sector")
@@ -22,8 +20,9 @@ public class GpsSectorController {
         // gps 데이터 전달받음
         // 해당 HashMap에서 이전 섹터 삭제, 새 섹터 입력
         // 5s 전체채팅 요청
-        gpsService.changeUserSector(DTO.getBeforeGpsKey(), DTO.getNowGpsKey(), sessionId, DTO.getAccountInfoDto());
         SimpAttributesContextHolder.currentAttributes().setAttribute("GPS", DTO.getNowGpsKey());
+        SimpAttributesContextHolder.currentAttributes().setAttribute("INFO", DTO.getAccountInfoDto());
+        gpsService.changeUserSector(DTO.getBeforeGpsKey(), DTO.getNowGpsKey(), sessionId);
     }
 
     @MessageMapping("/emoji")
