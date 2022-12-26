ff0846372edfec9bea7de226e9e678cd15cd6dd9
eora21 <eora21@naver.com>
2022-12-26 19:57:22 +0900

    feat: 🎸 init 메서드 추가, 세션 최대한 활용

--- backend/joalarm/src/main/java/com/cupid/joalarm/gpsSector/controller/GpsSectorController.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/gpsSector/controller/GpsSectorController.java
@@ -1,11 +1,15 @@
 package com.cupid.joalarm.gpsSector.controller;
 
-import com.cupid.joalarm.gpsSector.dto.EmojiDTO;
+import com.cupid.joalarm.gpsSector.dto.AccountInfoDto;
 import com.cupid.joalarm.gpsSector.dto.SectorDTO;
 import com.cupid.joalarm.gpsSector.service.GpsService;
+import com.cupid.joalarm.love.entity.Love;
+import com.cupid.joalarm.love.service.LoveService;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpAttributes;
 import org.springframework.messaging.simp.SimpAttributesContextHolder;
 import org.springframework.stereotype.Controller;
 
@@ -14,6 +18,20 @@ import org.springframework.stereotype.Controller;
 public class GpsSectorController {
 
     private final GpsService gpsService;
+    private final LoveService loveService;
+
+    @MessageMapping("/init")
+    public void init(@Header("simpSessionId") Long accountSeq) {
+        SimpAttributes simpAttributes = SimpAttributesContextHolder.currentAttributes();
+        simpAttributes.setAttribute("SEQ", accountSeq);
+        simpAttributes.setAttribute("INFO", createInfo(loveService.getLove(accountSeq)));
+    }
+
+    private AccountInfoDto createInfo(Optional<Love> love) {
+        AccountInfoDto accountInfoDto = new AccountInfoDto();
+        love.ifPresent(accountInfoDto::update);
+        return accountInfoDto;
+    }
 
     @MessageMapping("/sector")
     public void sector(@Header("simpSessionId") String sessionId, SectorDTO DTO) {
@@ -21,14 +39,6 @@ public class GpsSectorController {
         // 해당 HashMap에서 이전 섹터 삭제, 새 섹터 입력
         // 5s 전체채팅 요청
         SimpAttributesContextHolder.currentAttributes().setAttribute("GPS", DTO.getNowGpsKey());
-        SimpAttributesContextHolder.currentAttributes().setAttribute("INFO", DTO.getAccountInfoDto());
         gpsService.changeUserSector(DTO.getBeforeGpsKey(), DTO.getNowGpsKey(), sessionId);
-    }
-
-    @MessageMapping("/emoji")
-    public void sector(@Header("simpSessionId") String sessionId, EmojiDTO DTO) {
-        // 해당 HashMap에서 섹터 -> pk -> 이모지 수정
-        // 5s 전체채팅 요청
-        gpsService.changeUserEmoji(DTO.getGpsKey(), sessionId, DTO.getEmojiURL());
     }
 }
