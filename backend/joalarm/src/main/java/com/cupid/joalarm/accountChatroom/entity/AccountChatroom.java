60d27b9ff5feb117bafd965249d0869a0f9e8d4e
eora21 <eora21@naver.com>
2023-01-03 21:53:00 +0900

    feat: 🎸 현재 시간을 기반으로 랜덤한 이름을 저장하도록 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/entity/AccountChatroom.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/entity/AccountChatroom.java
@@ -2,6 +2,8 @@ package com.cupid.joalarm.accountChatroom.entity;
 
 import com.cupid.joalarm.base.entity.BaseTimeEntity;
 import com.cupid.joalarm.chat.entity.Chat;
+import com.cupid.joalarm.chatroom.util.RandomNameCreator;
+import java.time.LocalDateTime;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -22,7 +24,8 @@ public class AccountChatroom extends BaseTimeEntity {
     @EmbeddedId
     private AccountChatroomEmbedded accountChatroomEmbedded;
 
-    private String name;
+    @Builder.Default
+    private String name = RandomNameCreator.newName(LocalDateTime.now().getNano());
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "last_view_chat_seq")
