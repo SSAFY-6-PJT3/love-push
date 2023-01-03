cf1a264844590b1f2c663eee3f9ee86f8d830c08
eora21 <eora21@naver.com>
2023-01-03 22:15:30 +0900

    refactor: 💡 정수값 범위 중 랜덤값을 반영하도록 변경

--- backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/entity/AccountChatroom.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/entity/AccountChatroom.java
@@ -3,7 +3,6 @@ package com.cupid.joalarm.accountChatroom.entity;
 import com.cupid.joalarm.base.entity.BaseTimeEntity;
 import com.cupid.joalarm.chat.entity.Chat;
 import com.cupid.joalarm.chatroom.util.RandomNameCreator;
-import java.time.LocalDateTime;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -21,11 +20,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 public class AccountChatroom extends BaseTimeEntity {
+
     @EmbeddedId
     private AccountChatroomEmbedded accountChatroomEmbedded;
 
     @Builder.Default
-    private String name = RandomNameCreator.newName(LocalDateTime.now().getNano());
+    private String name = RandomNameCreator.newName((int) (Math.random() * Integer.MAX_VALUE));
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "last_view_chat_seq")
