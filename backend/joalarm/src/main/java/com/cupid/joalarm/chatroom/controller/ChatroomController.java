6125a7197a89d0cc7fdf1201f0c9fa04d86c819a
eora21 <eora21@naver.com>
2023-01-06 18:17:48 +0900

    feat: 🎸 채팅방을 생성 후 메시지를 전파하는 메서드 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/controller/ChatroomController.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/chatroom/controller/ChatroomController.java
@@ -1,7 +1,10 @@
 package com.cupid.joalarm.chatroom.controller;
 
+import com.cupid.joalarm.account.entity.Account;
+import com.cupid.joalarm.account.service.AccountService;
+import com.cupid.joalarm.accountChatroom.dto.AccountChatroomDto;
+import com.cupid.joalarm.accountChatroom.entity.AccountChatroom;
 import com.cupid.joalarm.accountChatroom.service.AccountChatroomService;
-import com.cupid.joalarm.chatroom.dto.CreateChatroomDto;
 import com.cupid.joalarm.chatroom.entity.Chatroom;
 import com.cupid.joalarm.chatroom.service.ChatroomService;
 import com.cupid.joalarm.message.Message;
@@ -10,7 +13,6 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Api("chatroom 관련 기능")
@@ -21,6 +23,7 @@ public class ChatroomController {
     private final ChatroomService chatRoomService;
     private final AccountChatroomService accountChatroomService;
     private final Message message;
+    private final AccountService accountService;
 
     @GetMapping
     @ApiOperation(value = "채팅방 조회", notes = "본인이 속한 모든 채팅방과 해당 채팅방의 최근 채팅 한개를 가져옵니다.")
@@ -28,13 +31,18 @@ public class ChatroomController {
         return new ResponseEntity<>(accountChatroomService.findMyChatroomList(accountSeq), HttpStatus.OK);
     }
 
-    @PostMapping()
-    @ApiOperation(value = "채팅방 생성", notes = "다른 유저와의 채팅방을 생성합니다.")
-    public ResponseEntity<?> createRoom(@RequestBody CreateChatroomDto DTO) {
-        Chatroom chatroom = chatRoomService.CreateChatRoom(DTO.getSendAccountSeq(), DTO.getReceiveAccountSeq());
-        message.createChatroom(DTO.getSendAccountSeq(), chatroom.getSeq());
-        message.createChatroom(DTO.getReceiveAccountSeq(), chatroom.getSeq());
-        return new ResponseEntity<>(HttpStatus.OK);
+    public Chatroom createChatroom(long sendAccountSeq, long receiveAccountSeq) {
+        Chatroom chatroom = chatRoomService.createChatroom();
+        Account sendAccount = accountService.findAccountBySeq(sendAccountSeq);
+        Account receiveAccount = accountService.findAccountBySeq(receiveAccountSeq);
+        AccountChatroom sendAccountChatroom = accountChatroomService.createAccountChatroom(sendAccount, chatroom);
+        AccountChatroom receiveAccountChatroom = accountChatroomService.createAccountChatroom(receiveAccount, chatroom);
+        message.createChatroom(sendAccount.getAccountSeq(),
+                AccountChatroomDto.fromEntity(chatroom.getSeq(), sendAccountChatroom));
+        message.createChatroom(sendAccount.getAccountSeq(),
+                AccountChatroomDto.fromEntity(chatroom.getSeq(), receiveAccountChatroom));
+
+        return chatroom;
     }
 
     @PostMapping("view")
