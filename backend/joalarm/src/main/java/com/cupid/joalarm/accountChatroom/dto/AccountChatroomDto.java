f2aa31d1dec52051de240af28815cf5dc76113e8
eora21 <eora21@naver.com>
2023-01-06 18:14:29 +0900

    feat: 🎸 엔티티를 Dto로 변환하는 메서드 추가

--- backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/dto/AccountChatroomDto.java
+++ backend/joalarm/src/main/java/com/cupid/joalarm/accountChatroom/dto/AccountChatroomDto.java
@@ -1,5 +1,6 @@
 package com.cupid.joalarm.accountChatroom.dto;
 
+import com.cupid.joalarm.accountChatroom.entity.AccountChatroom;
 import com.cupid.joalarm.chat.dto.ChatDto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,4 +12,13 @@ public class AccountChatroomDto {
     private String name;
     private ChatDto lastMessage;
     private Long unreadChatCnt;
+
+    public static AccountChatroomDto fromEntity(long chatroomSeq, AccountChatroom accountChatroom) {
+        return new AccountChatroomDto(
+                chatroomSeq,
+                accountChatroom.getName(),
+                new ChatDto(),
+                0L
+        );
+    }
 }
